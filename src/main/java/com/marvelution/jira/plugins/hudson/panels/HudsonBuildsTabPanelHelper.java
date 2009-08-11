d0f5b67ed0e7314a3bafecbd8514dfb6076c541f
Mark Rekveld <markrekveld@marvelution.com>
2009-08-11 21:18:54 +0000

    [MARVJIRAHUDSON-18] Implemented sub tabs for Project and Version tab panels
    git-svn-id: http://svn.marvelution.com/marvelution/marvelution-jira-hudson-integration/trunk/jira-hudson-plugin@945 079872ed-eb71-41de-a42c-5a92345b582c

--- src/main/java/com/marvelution/jira/plugins/hudson/panels/HudsonBuildsTabPanelHelper.java
+++ src/main/java/com/marvelution/jira/plugins/hudson/panels/HudsonBuildsTabPanelHelper.java
@@ -19,13 +19,20 @@
 
 package com.marvelution.jira.plugins.hudson.panels;
 
+import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+
+import webwork.action.ActionContext;
+
+import com.atlassian.core.util.collection.EasyList;
 import com.atlassian.jira.bc.project.component.ProjectComponent;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.project.version.Version;
+import com.atlassian.jira.util.ParameterUtils;
 import com.atlassian.plugin.webresource.WebResourceManager;
 import com.marvelution.jira.plugins.hudson.service.HudsonServerManager;
 
@@ -37,6 +44,14 @@ import com.marvelution.jira.plugins.hudson.service.HudsonServerManager;
 public class HudsonBuildsTabPanelHelper {
 
 	public static final String HUDSON_BUILD_PLUGIN = "com.marvelution.jira.plugins.hudson";
+
+	public static final String SELECTED_SUB_TAB_KEY = "selectedSubTab";
+
+	public static final String SUB_TAB_BUILD_BY_PLAN = "buildByPlan";
+
+	public static final String SUB_TAB_BUILD_BY_ISSUE = "buildByIssue";
+
+	public static final List<?> SUB_TABS = EasyList.build(SUB_TAB_BUILD_BY_PLAN, SUB_TAB_BUILD_BY_ISSUE);
 
 	private final HudsonServerManager serverManager;
 
@@ -63,11 +78,14 @@ public class HudsonBuildsTabPanelHelper {
 	 * 
 	 * @param velocityParams {@link Map} of existing velocity parameters to extend
 	 * @param project the {@link Project} to prepare the velocity parameters for
+	 * @param baseLinkUrl the base url for links to other tab panels
+	 * @param subTabs available sub tabs
 	 */
-	public void prepareVelocityParameters(Map<String, Object> velocityParams, Project project) {
+	public void prepareVelocityParameters(Map<String, Object> velocityParams, Project project, String baseLinkUrl,
+					List<?> subTabs) {
 		velocityParams.put("projectKey", project.getKey());
 		velocityParams.put("hudsonServer", serverManager.getServerByJiraProject(project));
-		prepareVelocityParameters(velocityParams, "projectKey=" + project.getKey(), "project");
+		prepareVelocityParameters(velocityParams, "projectKey=" + project.getKey(), "project", baseLinkUrl, subTabs);
 	}
 
 	/**
@@ -75,11 +93,14 @@ public class HudsonBuildsTabPanelHelper {
 	 * 
 	 * @param velocityParams {@link Map} of existing velocity parameters to extend
 	 * @param version the {@link Version} to prepare the velocity parameters for
+	 * @param baseLinkUrl the base url for links to other tab panels
+	 * @param subTabs available sub tabs
 	 */
-	public void prepareVelocityParameters(Map<String, Object> velocityParams, Version version) {
+	public void prepareVelocityParameters(Map<String, Object> velocityParams, Version version, String baseLinkUrl,
+					List<?> subTabs) {
 		velocityParams.put("versionId", version.getId());
 		velocityParams.put("hudsonServer", serverManager.getServerByJiraProject(version.getProjectObject()));
-		prepareVelocityParameters(velocityParams, "versionId=" + version.getId(), "version");
+		prepareVelocityParameters(velocityParams, "versionId=" + version.getId(), "version", baseLinkUrl, subTabs);
 	}
 
 	/**
@@ -91,7 +112,7 @@ public class HudsonBuildsTabPanelHelper {
 	public void prepareVelocityParameters(Map<String, Object> velocityParams, Issue issue) {
 		velocityParams.put("issueKey", issue.getKey());
 		velocityParams.put("hudsonServer", serverManager.getServerByJiraProject(issue.getProjectObject()));
-		prepareVelocityParameters(velocityParams, "issueKey=" + issue.getKey(), "issue");
+		prepareVelocityParameters(velocityParams, "issueKey=" + issue.getKey(), "issue", null, null);
 	}
 
 	/**
@@ -104,7 +125,7 @@ public class HudsonBuildsTabPanelHelper {
 		velocityParams.put("componentId", component.getId());
 		final Project project = projectManager.getProjectObj(component.getProjectId());
 		velocityParams.put("hudsonServer", serverManager.getServerByJiraProject(project));
-		prepareVelocityParameters(velocityParams, "componentId=" + component.getId(), "component");
+		prepareVelocityParameters(velocityParams, "componentId=" + component.getId(), "component", null, null);
 	}
 
 	/**
@@ -113,14 +134,55 @@ public class HudsonBuildsTabPanelHelper {
 	 * @param velocityParams the existing {@link Map} of velocity parameters to extend
 	 * @param querySection the query section for invoking the ViewHudsonServerPanelContent page
 	 * @param moduleKey the module key of the panel
+	 * @param baseLinkUrl the base url for links to other tab panels, may be <code>null</code> to not add it to the
+	 *            velocity parameters
+	 * @param subTabs available sub tabs
 	 */
-	private void prepareVelocityParameters(Map<String, Object> velocityParams, String querySection, String moduleKey) {
+	private void prepareVelocityParameters(Map<String, Object> velocityParams, String querySection, String moduleKey,
+					String baseLinkUrl, List<?> subTabs) {
 		webResourceManager.requireResource(HUDSON_BUILD_PLUGIN + ":tabpanel-css");
 		webResourceManager.requireResource("jira.webresources:prototype");
 		velocityParams.put("querySection", querySection);
 		velocityParams.put("moduleKey", moduleKey);
 		velocityParams.put("baseResourceUrl", "/download/resources/" + HUDSON_BUILD_PLUGIN + ":hudson-" + moduleKey
 			+ "-tabpanel");
+		if (baseLinkUrl != null) {
+			velocityParams.put("baseLinkUrl", baseLinkUrl + HUDSON_BUILD_PLUGIN + ":hudson-" + moduleKey
+				+ "-tabpanel");
+		}
+		velocityParams.put("selectedSubTab", retrieveFromRequestOrSession("selectedSubTab", SUB_TAB_BUILD_BY_PLAN));
+		velocityParams.put("availableTabs", subTabs);
+	}
+
+	/**
+	 * Get a parameter from the current Request or current Session. If it is not set the default value will be
+	 * returned.
+	 * 
+	 * @param requestKey the parameter key to get
+	 * @param defaultValue the default value if the parameter is not set
+	 * @return the value of the parameter, or the given default value
+	 */
+	public static String retrieveFromRequestOrSession(String requestKey, String defaultValue) {
+		final String value = retrieveFromRequestOrSession(requestKey);
+		return ((value != null) ? value : defaultValue);
+	}
+
+	/**
+	 * Get a parameter value from the current Request or Session.
+	 * 
+	 * @param requestKey the parameter key to get
+	 * @return the value of the parameter
+	 */
+	@SuppressWarnings("unchecked")
+	public static String retrieveFromRequestOrSession(String requestKey) {
+		final String sessionKey = HudsonBuildsTabPanelHelper.HUDSON_BUILD_PLUGIN + "." + requestKey;
+		final String paramFromRequest = ParameterUtils.getStringParam(ActionContext.getParameters(), requestKey);
+		final Map<String, String> session = ActionContext.getSession();
+		if (StringUtils.isNotBlank(paramFromRequest)) {
+			session.put(sessionKey, paramFromRequest);
+			return paramFromRequest;
+		}
+		return ((String) session.get(sessionKey));
 	}
 
 }
