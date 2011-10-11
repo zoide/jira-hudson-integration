795b3f4289b10798c8f17e3b39917759efce91e8
Mark Rekveld <markrekveld@marvelution.com>
2011-10-11 13:40:18 +0200

    MARVADMIN-135 Updated all license headers

--- jira-hudson-plugin/src/main/java/com/marvelution/jira/plugins/hudson/web/action/admin/associations/UpdateAssociation.java
+++ jira-hudson-plugin/src/main/java/com/marvelution/jira/plugins/hudson/web/action/admin/associations/UpdateAssociation.java
@@ -1,7 +1,6 @@
 /*
-
- * Licensed to Marvelution under one or more contributor license 
- * agreements.  See the NOTICE file distributed with this work 
+ * Licensed to Marvelution under one or more contributor license
+ * agreements.  See the NOTICE file distributed with this work
  * for additional information regarding copyright ownership.
  * Marvelution licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
